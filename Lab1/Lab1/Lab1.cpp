ac780363c5184761637a6cff0b08852ef4dce4aa
bandalak.olexandr@unicyb.kiev.ua <bandalak.olexandr@unicyb.kiev.ua>
2020-01-23 19:28:56 +0200

    потрібно налаштувати пошук

--- Lab1/Lab1/Lab1.cpp
+++ Lab1/Lab1/Lab1.cpp
@@ -20,21 +20,21 @@ struct info_monster
     unsigned short int damage;//1-1000
     double chance;//0-1 
     char type_of_attack[100];//збільшити пошкодження, повторити атаку,вилікувати себе, паралізувати супротивника; 
-    //tm *time_info;  //time_t seconds = time(NULL); tm* timeinfo = localtime(&seconds); cout << "Current Datetime:" << asctime(timeinfo) << endl;   
+    char time_info[26];  //time_t seconds = time(NULL); tm* timeinfo = localtime(&seconds); cout << "Current Datetime:" << asctime(timeinfo) << endl;   
 };
-std:: vector<info_monster> all_monster;
-void my_cls()
+std:: vector<info_monster> all_monsters;//місце де зберігаються всі монстри
+void my_cls()//очищує екран і виводить назву програми
 {
     system("CLS");
     cout << "<The Forest of Monsters>"<< endl << endl;
 }
-int set_id()
+int set_id()//створює унікальний код
 {
     srand(time(0));
     long int id_monster=(rand()%90000 + 10000);
     return id_monster;
 }
-void add_new_monster()
+void add_new_monster()//створює нового монстра
 {
     my_cls();
     cout << "Створіть власного монстра!\n";
@@ -47,18 +47,90 @@ void add_new_monster()
     cin >> new_monster.damage;
     cout << "Введіть шанс монстра виконати спеціальну атаку(0.00 - 1.00): ";
     cin >> new_monster.chance;
-    cout << "Введіть один із можливих типів спеціальної атаки монстра(збільшити пошкодження, повторити атаку, вилікувати себе, паралізувати супротивника): ";
+    cout << "Введіть один із можливих типів спеціальної атаки монстра(збільшити пошкодження,\n" 
+         <<"повторити атаку, вилікувати себе, паралізувати супротивника): ";
     scanf_s("%100s", new_monster.type_of_attack, (unsigned)_countof(new_monster.type_of_attack));
     new_monster.id = set_id();
     my_cls();
     cout << "Новий монстр створений!\nЙого персональний ID: " << new_monster.id << endl;
-    //time_t seconds = time(NULL);//тут
-    //new_monster.time_info = localtime(&seconds);//тут
-    all_monster.push_back(new_monster);
-   // cout << "Дата та час створення: " << asctime(new_monster.time_info )<< endl;
+    time_t seconds = time(NULL);
+    ctime_s(new_monster.time_info,26,&seconds);
+    all_monsters.push_back(new_monster);
+    cout << "Дата та час створення: " << new_monster.time_info<< endl;
     cout << "Нажміть '0', щоб продовжити.\n";
     not_null:
     if (_getch() != '0') goto not_null;  
+}
+void write_monsters(std::vector<int> monsters_nombers)//виводить дані монстра на еран
+{
+    cout << "Знайдені монстри:\n";
+    for (int i = 0; i < monsters_nombers.size(); i++) 
+    {
+        cout << i + 1 << ".\n";
+        cout << all_monsters[monsters_nombers[i]].id << endl;
+        cout << all_monsters[monsters_nombers[i]].name << endl;
+        cout << all_monsters[monsters_nombers[i]].xp << endl;
+        cout << all_monsters[monsters_nombers[i]].damage << endl;
+        cout << all_monsters[monsters_nombers[i]].chance << endl;
+        cout << all_monsters[monsters_nombers[i]].type_of_attack << endl;
+        cout << all_monsters[monsters_nombers[i]].time_info;
+    }
+}
+void faund_name()//пошук не працює
+{
+    my_cls();
+    cout << "Введіть повне ім'я або фрагмент імені монстра: \n";
+    char fragment_name[200];
+    std:: vector <int> nombers_monsters;
+    scanf_s("%200s", fragment_name, (unsigned)_countof(fragment_name));
+    for (int i = 0; i < all_monsters.size(); i++)
+    {
+        char* p;
+        p = all_monsters[i].name;
+        cout << "p[" << i << "] = " << p << endl;
+        int fragment_size = strlen(fragment_name), name_size = strlen(all_monsters[i].name);//неправильно довжина
+        cout << "fragment_name = " << fragment_name << "||||||"<<endl;
+        cout << "p = " << p << "||||||" << endl;
+        cout << "fragment_size = " << fragment_size << endl;
+        cout << "name_size = " << fragment_size << endl;
+        if (fragment_size > name_size) break;
+        else
+        {
+            bool flag = true;
+            for (int j = 0,k = 0,j_save = 0; j < name_size,k < fragment_size,j >= k,j_save < name_size; j++)
+            {
+                if (p[j] == fragment_name[k])
+                {
+                    k++;
+                    cout << "k = " << k << endl;
+                    flag = true;
+                }
+                else
+                {
+                    k = 0;
+                    j_save++;
+                    cout << "j_save = " << j_save << endl;
+                    j = j_save;
+                    flag = false;
+                }
+                cout << "j =  " << j << endl;
+            }
+            if (flag)
+            {
+                nombers_monsters.push_back(i);
+                break;
+            }
+        }
+    }
+    int test;
+    cin >> test;
+    my_cls();
+    if (nombers_monsters.size() == 0) cout << "Монстр не знайдений!\n";
+    else write_monsters(nombers_monsters);
+    cout << "Нажміть '0',щоб вийти.\n";
+    not_null:
+    if (_getch() != '0') goto not_null;
+
 }
 void Interactive_dialog_mode()
 {
@@ -77,7 +149,24 @@ void Interactive_dialog_mode()
     break;
     case '2':
     {
+        my_cls();
+        cout << "Виберіть режим пошуку монстра:\n1)Пошук по імені.\n2)Пошук по ID.\n";
+        found_monstr:
+        switch (_getch())
+        {
+            case '1':
+            {
+                faund_name();
+                goto next;
+            }
+            break;
+            case '2':
+            {
 
+            }
+            break;
+            default: goto found_monstr;
+        }
     }
     break;
     case '0':
