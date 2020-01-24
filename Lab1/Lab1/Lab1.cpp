3e112988f0eae1ae2ecce66aa28b84845c0627ca
bandalak.olexandr@unicyb.kiev.ua <bandalak.olexandr@unicyb.kiev.ua>
2020-01-24 22:24:13 +0200

    ПОшук працює добре, проблеми в додаванні монстра.  нада найти заміну gets

--- Lab1/Lab1/Lab1.cpp
+++ Lab1/Lab1/Lab1.cpp
@@ -8,6 +8,7 @@
 #include <cstring>
 #include <vector>
 #include <stdio.h>
+#include <string>
 using std::cout;
 using std::cin;
 using std::endl;
@@ -19,7 +20,7 @@ struct info_monster
     unsigned int xp;//1-50000
     unsigned short int damage;//1-1000
     double chance;//0-1 
-    char type_of_attack[100];//збільшити пошкодження, повторити атаку,вилікувати себе, паралізувати супротивника; 
+    std:: string type_of_attack;//збільшити пошкодження, повторити атаку,вилікувати себе, паралізувати супротивника; 
     char time_info[26];  //time_t seconds = time(NULL); tm* timeinfo = localtime(&seconds); cout << "Current Datetime:" << asctime(timeinfo) << endl;   
 };
 std:: vector<info_monster> all_monsters;//місце де зберігаються всі монстри
@@ -37,58 +38,120 @@ int set_id()//створює унікальний код
 void add_new_monster()//створює нового монстра
 {
     my_cls();
-    cout << "Створіть власного монстра!\n";
+    cout << "Create your own monster!\n";
     info_monster new_monster;
-    cout << "Введіть ім'я монстра: ";
-    scanf_s("%200s", new_monster.name, (unsigned)_countof(new_monster.name));
-    cout << "Введіть кількість одиниць здоров'я монстра(1-50000): ";
-    cin >> new_monster.xp;
-    cout << "Введіть кількість одиниць атаки монстра(1-1000): ";
-    cin >> new_monster.damage;
-    cout << "Введіть шанс монстра виконати спеціальну атаку(0.00 - 1.00): ";
-    cin >> new_monster.chance;
-    cout << "Введіть один із можливих типів спеціальної атаки монстра(збільшити пошкодження,\n" 
-         <<"повторити атаку, вилікувати себе, паралізувати супротивника): ";
-    scanf_s("%100s", new_monster.type_of_attack, (unsigned)_countof(new_monster.type_of_attack));
+    cout << "Enter a monster name: ";
+    scanf_s("%200s", new_monster.name, (unsigned)_countof(new_monster.name));//при вводі двох слів зависає
+    while (true)
+    {
+        cout << "Enter the number of monster health units (1-50000): ";
+        int temp_xp;
+        cin >> temp_xp;
+        if ((temp_xp < 1) || (temp_xp > 50000))
+        {
+            my_cls();
+            cout << "Create your own monster!\n";
+            cout << "Enter a monster name: " << new_monster.name << endl;
+        }
+        else
+        {
+            new_monster.xp = temp_xp;
+            break;
+        }      
+    }
+    while (true)
+    {
+        cout << "Enter the number of monster attack units (1-1000): ";
+        int temp_damage;
+        cin >> temp_damage;
+        if ((temp_damage < 1) || (temp_damage > 1000))
+        {
+            my_cls();
+            cout << "Create your own monster!\n";
+            cout << "Enter a monster name: " << new_monster.name << endl;
+            cout << "Enter the number of monster health units (1-50000): " << new_monster.xp << endl;
+        }
+        else
+        {
+            new_monster.damage = temp_damage;
+            break;
+        }
+    }
+    while (true)
+    {
+        cout << "Enter the monster's chance to launch a special attack (0.00 - 1.00): ";
+        double temp_chance;
+        cin >> temp_chance;
+        if ((temp_chance < 0) || (temp_chance > 1))
+        {
+            my_cls();
+            cout << "Create your own monster!\n";
+            cout << "Enter a monster name: " << new_monster.name << endl;
+            cout << "Enter the number of monster health units (1-50000): " << new_monster.xp << endl;
+            cout << "Enter the number of monster attack units (1-1000): " << new_monster.damage << endl;
+        }
+        else
+        {
+            new_monster.chance = temp_chance;
+            break;
+        }
+    }
+    cout << "Choose one of the possible types of special monster attack:\n1)Increase damage.\n2)Repeat the attack.\n"
+         << "3)Cure yourself.\n4)Paralyze the enemy.\n";
+types_attack:
+    switch (_getch())
+    {
+    case'1': new_monster.type_of_attack = "Increase damage";
+        break;
+    case'2': new_monster.type_of_attack = "Repeat the attack";
+        break;
+    case '3': new_monster.type_of_attack = "Cure yourself";
+        break;
+    case'4': new_monster.type_of_attack = "Paralyze the enemy";
+        break;
+    default: goto types_attack;
+        break;
+    }
     new_monster.id = set_id();
     my_cls();
-    cout << "Новий монстр створений!\nЙого персональний ID: " << new_monster.id << endl;
+    cout << "New monster created!\nHis personal ID: " << new_monster.id << endl;
     time_t seconds = time(NULL);
     ctime_s(new_monster.time_info,26,&seconds);
     all_monsters.push_back(new_monster);
-    cout << "Дата та час створення: " << new_monster.time_info<< endl;
-    cout << "Нажміть '0', щоб продовжити.\n";
+    cout << "Creation date and time: " << new_monster.time_info<< endl;
+    cout << "Press '0' to continue.\n";
     not_null:
     if (_getch() != '0') goto not_null;  
 }
 void write_monsters(std::vector<int> monsters_nombers)//виводить дані монстра на еран
 {
-    cout << "Знайдені монстри:\n";
+    cout << "Monsters found:\n";
     for (int i = 0; i < monsters_nombers.size(); i++) 
     {
         cout << i + 1 << ".\n";
-        cout << all_monsters[monsters_nombers[i]].id << endl;
-        cout << all_monsters[monsters_nombers[i]].name << endl;
-        cout << all_monsters[monsters_nombers[i]].xp << endl;
-        cout << all_monsters[monsters_nombers[i]].damage << endl;
-        cout << all_monsters[monsters_nombers[i]].chance << endl;
-        cout << all_monsters[monsters_nombers[i]].type_of_attack << endl;
-        cout << all_monsters[monsters_nombers[i]].time_info;
+        cout <<"ID: " << all_monsters[monsters_nombers[i]].id << endl;
+        cout << "Name: " <<all_monsters[monsters_nombers[i]].name << endl;
+        cout << "XP: " <<all_monsters[monsters_nombers[i]].xp << endl;
+        cout << "Damage: " << all_monsters[monsters_nombers[i]].damage << endl;
+        cout << "Chance to launch a special attack: " << all_monsters[monsters_nombers[i]].chance << endl;
+        cout << "Type of special monster attack: " << all_monsters[monsters_nombers[i]].type_of_attack << endl;
+        cout << "Creation date and time: " << all_monsters[monsters_nombers[i]].time_info;
     }
 }
-void faund_name()//пошук не працює
+void find_name()//пошук монстра по імені
 {
     my_cls();
-    cout << "Введіть повне ім'я або фрагмент імені монстра: \n";
+    cout << "Enter the full name or snippet of the monster name:\n";
     char fragment_name[200];
     std:: vector <int> nombers_monsters;
-    scanf_s("%200s", fragment_name, (unsigned)_countof(fragment_name));
+    scanf_s("%200s",fragment_name, (unsigned)_countof(fragment_name));
+    int fragment_size = strlen(fragment_name);
     for (int i = 0; i < all_monsters.size(); i++)
     {
         char* p;
         p = all_monsters[i].name;
         cout << "p[" << i << "] = " << p << endl;
-        int fragment_size = strlen(fragment_name), name_size = strlen(all_monsters[i].name);
+        int name_size = strlen(all_monsters[i].name);
         cout << "fragment_name = " << fragment_name << "||||||"<<endl;
         cout << "p = " << p << "||||||" << endl;
         cout << "fragment_size = " << fragment_size << endl;
@@ -120,12 +183,10 @@ void faund_name()//пошук не працює
             if (flag) nombers_monsters.push_back(i);
         }
     }
-    int test;
-    cin >> test;
     my_cls();
-    if (nombers_monsters.size() == 0) cout << "Монстр не знайдений!\n";
+    if (nombers_monsters.size() == 0) cout << "Monster not found!\n";
     else write_monsters(nombers_monsters);
-    cout << "Нажміть '0',щоб вийти.\n";
+    cout << "Press '0' to exit.\n";
     not_null:
     if (_getch() != '0') goto not_null;
 
@@ -134,8 +195,8 @@ void Interactive_dialog_mode()
 {
     next:
     my_cls();
-    cout << "Меню:\n";
-    cout << "1)Додати нового монстра.\n2)Знайти існуючого монстра.\n0)Назад.\n";
+    cout << "Menu:\n";
+    cout << "1)Add a new monster.\n2)Find an existing monster.\n0)Back.\n";
     menu:
     switch (_getch())
     {
@@ -148,13 +209,13 @@ void Interactive_dialog_mode()
     case '2':
     {
         my_cls();
-        cout << "Виберіть режим пошуку монстра:\n1)Пошук по імені.\n2)Пошук по ID.\n";
+        cout << "Select Monster Search Mode:\n1)Search by Name.\n2)Search by ID.\n";
         found_monstr:
         switch (_getch())
         {
             case '1':
             {
-                faund_name();
+                find_name();
                 goto next;
             }
             break;
@@ -175,10 +236,9 @@ void Interactive_dialog_mode()
 }
 int main()
 {
-    SetConsoleOutputCP(1251);
     next:
     my_cls();
-    cout << "Виберіть режим роботи програми:\n1)Інтерактивний діалоговий режим.\n2)Демонстраційний режим.\n3)Автоматичний режим “benchmark”\n0)Вихід.\n";
+    cout << "Select the application mode:\n1)Interactive dialog mode.\n2)Demo mode.\n3)Automatic benchmark mode.\n0)Exit.\n";
     mode:
     switch(_getch())
     {
@@ -201,7 +261,7 @@ int main()
         case '0':
         {
             my_cls();
-            cout << "Вихід...";
+            cout << "Exit...";
             return 0;
         }
         break;
