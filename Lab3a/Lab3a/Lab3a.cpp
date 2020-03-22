b60fd481c355aa41f0d75470e66a27a43f085681
bandalak.olexandr@unicyb.kiev.ua <bandalak.olexandr@unicyb.kiev.ua>
2020-03-22 22:20:01 +0200

    залишилося добавити демонстраційний режим

--- Lab3a/Lab3a/Lab3a.cpp
+++ Lab3a/Lab3a/Lab3a.cpp
@@ -48,6 +48,7 @@ void test()
     long size;
     cout << "Enter size:";
     cin >> size;
+    if (size <= 0) return;
     int* array = array_generator(size);
     array[size / 2] = 31000;
     write_array(array, size);
@@ -71,9 +72,21 @@ void add_to_end_of_file(T& value, const std::string& name_of_value,const std::st
     file << name_of_value << " = " << value << "." << endl;
     file.close();
 }
-void add_result_to_file()
+void add_result_to_file(measurement_result result, const std::string& name_of_sort)
 {
-
+    add_to_end_of_file(result.size, "Size", name_of_sort);
+    add_to_end_of_file(result.time, "Time", name_of_sort);
+}
+void clear_result_files()
+{
+    const int number_of_sorts = 5;
+    std::string paths[number_of_sorts] = { "Bubble sort", "Quick sort", 
+        "Merge sort topdown", "Library sort", "Combined sort" };
+    for (int i = 0; i < number_of_sorts; i++)
+    {
+        std::ofstream file(paths[i]+".txt");
+        file.close();
+;   }
 }
 float measurement_sort(void sort(int*, long), int* array, long size, const std::string& name_of_sort)
 {
@@ -86,9 +99,13 @@ float measurement_sort(void sort(int*, long), int* array, long size, const std::
     sort(copy_array, size);
     the_end = std::chrono::high_resolution_clock::now();
     duration = the_end - the_start;
-    cout << "Time of " << name_of_sort << " = " << duration.count() << endl;
+    measurement_result result;
+    result.size = size;
+    result.time = duration.count();
+    add_result_to_file(result, name_of_sort);
+    cout << "Time of " << name_of_sort << " = " << result.time << endl;
     delete[] copy_array;
-    return duration.count();
+    return result.time;
 }
 float measurement_combined(int* array, long size, const int threshold)
 {
@@ -101,10 +118,15 @@ float measurement_combined(int* array, long size, const int threshold)
     sorting::combined_sort(copy_array, size, threshold);
     the_end = std::chrono::high_resolution_clock::now();
     duration = the_end - the_start;
-    cout << "Time of Combined sort = " << duration.count() << endl;
+    measurement_result result;
+    result.size = size;
+    result.time = duration.count();
+    add_result_to_file(result, "Combined sort");
+    add_to_end_of_file(threshold, "Threshold", "Combined sort");
     cout << "Threshold of Combined sort = " << threshold << endl;
+    cout << "Time of Combined sort = " << result.time << endl;  
     delete[] copy_array;
-    return duration.count(); 
+    return result.time;
 }
 long set_size(bool is_one_second, long& copy_size, long& coefficient)
 {
@@ -119,33 +141,52 @@ long set_size(bool is_one_second, long& copy_size, long& coefficient)
 }
 void benchmark_mode()
 {
+    clear_result_files();
     const int number_of_sorts = 4;
     void(*all_sorts[number_of_sorts])(int*, long) = { sorting::bubble_sort, sorting::quick_sort,
     sorting::merge_sort_topdown, sorting::library_sort };
     std::string name_of_sorts[number_of_sorts] = { "Bubble sort", "Quick sort", "Merge sort topdown", "Library sort" };
-    bool is_one_second = false, are_ten_seconds = false;
+    bool is_one_second = false, are_ten_seconds = false, long_bubble = false, once = true;
     long size = 1000;
     long copy_size = size, coefficient = 2;
-    int threshold = 0;
+    int max_threshold = 15;
     while (true)
     {     
         int* array = array_generator(size);
         float time;
-        cout << "\nSize of array = " << size << endl;
+        cout << "\nSize of array = " << size << endl << endl;
         for (int i = 0; i < number_of_sorts; i++)
         {
+            if (long_bubble && (i == 0)) continue;
             time = measurement_sort(all_sorts[i], array, size, name_of_sorts[i]);           
             if (time > 1) is_one_second = true;
-            if (time > 100) are_ten_seconds = true;
+            if(time < 1)
+            if ((time > 10)&&(i!=0)) are_ten_seconds = true;
+            if (time > 10) long_bubble = true;
         }
-        time = measurement_combined(array, size, threshold++);
-        if (time > 1) is_one_second = true;
-        if (time > 100) are_ten_seconds = true;
+        if (once&&long_bubble)
+        {
+            is_one_second = false;
+            copy_size = size;
+            coefficient = 2;
+            once = false;
+        }
+        cout << endl;
+        for (int j = 1; j < max_threshold; j++)
+        {
+            time = measurement_combined(array, size, j);
+            if (time > 1) is_one_second = true;
+            if (time > 10) are_ten_seconds = true;
+        }     
         if (are_ten_seconds) break;
         size = set_size(is_one_second, copy_size, coefficient);
         delete[] array;
         array = nullptr;
-    } 
+    }
+    cout << "\nResults of measurements of program in the following files:\n"
+        << name_of_sorts[0] + ".txt\n" << name_of_sorts[1] + ".txt\n"
+        << name_of_sorts[2] + ".txt\n" << name_of_sorts[3] + ".txt\n"
+        << "Combined sort.txt" << endl;
 }
 int main()
 {
