4e38b7d0a02fcd6fbac4f2d4cc6e3123cda2dd42
bandalak.olexandr@unicyb.kiev.ua <bandalak.olexandr@unicyb.kiev.ua>
2020-04-26 14:23:14 +0300

    Виправив деякі помилки

--- Lab5/Lab5/Lab5.cpp
+++ Lab5/Lab5/Lab5.cpp
@@ -6,18 +6,16 @@ struct Graph_node
 {
     bool contiguity;
     int weight_coefficient;
-    bool was_verified;
+
     Graph_node()
     {
         contiguity = false;
         weight_coefficient = 0;
-        was_verified = false;
     }
     Graph_node(bool contiguity, int weight_coefficient)
     {
         this->contiguity = contiguity;
         this->weight_coefficient = weight_coefficient;
-        was_verified = false;
     }
 };
 struct Graph_matrix
@@ -44,37 +42,63 @@ private:
         }
         indexes.push_back(index);
     }
-    void checking_the_connectivity_of_graph_current(std::vector<std::size_t>& indexes, std::size_t index)
+    template<class T>
+    void swap(T& a, T& b)
     {
-        for (std::size_t j = 0; j < number_of_vertex; j++)
+        T temp = a;
+        a = b;
+        b = temp;
+    }
+    std::vector<std::size_t> create_numbers(std::size_t index, bool mode)
+    {
+        std::vector<std::size_t> numbers;
+        std::vector<Graph_node> line = array[index];
+        for (std::size_t i = 0; i < number_of_vertex; i++) numbers.push_back(i);
+        if(!mode) return numbers;
+        for (std::size_t i = 0; i < number_of_vertex - 1; i++)
         {
-            if (array[index][j].contiguity)
+            for (std::size_t j = 0; j < (number_of_vertex - 1 - i); j++)
             {
-                add_index(indexes, index);
-                if (index == j) continue;
-                if (!array[index][j].was_verified)
+                if (line[j].weight_coefficient > line[j + 1].weight_coefficient)
                 {
-                    array[index][j].was_verified = true;
-                    checking_the_connectivity_of_graph_current(indexes, j);
+                    swap(line[j].weight_coefficient, line[j + 1].weight_coefficient);
+                    swap(numbers[j], numbers[j + 1]);
+                }
+            }
+        }
+        return numbers;
+    }
+    bool add_vertex(std::vector<std::size_t> indexes, std::size_t index_i, std::size_t index_j)
+    {
+        for (std::size_t i = 0; i < indexes.size(); i++)
+        {
+            if (indexes[i] == index_j)
+            {
+                return false;
+            }
+        }       
+        return true;
+    }
+    void is_connected_current(std::vector<std::size_t>& indexes, std::size_t index, bool mode)
+    {
+        std::vector<std::size_t> numbers = create_numbers(index, mode);
+        for (std::size_t j = 0; j < number_of_vertex; j++)
+        {            
+            if (array[index][numbers[j]].contiguity)
+            {
+                add_index(indexes, index);              
+                if (index == numbers[j]) continue;
+                if (add_vertex(indexes, index, numbers[j]))
+                {
+                    add_index(indexes, numbers[j]);
+                    is_connected_current(indexes, numbers[j], mode);
                 }
             }
         }
     }
-    void verified_false()
+    bool is_connected(std::vector<std::size_t>& indexes, bool mode)
     {
-        for (std::size_t i = 0; i < number_of_vertex; i++)
-        {
-            for (std::size_t j = 0; i < number_of_vertex; i++)
-            {
-                array[i][j].was_verified = false;
-            }
-        }
-    }
-    bool is_connected(std::vector<std::size_t>& indexes)
-    {
-        array[0][0].was_verified = true;
-        checking_the_connectivity_of_graph_current(indexes, 0);
-        verified_false();
+        is_connected_current(indexes, 0, mode);
         if (indexes.size() == number_of_vertex)
         {
             return true;
@@ -142,11 +166,13 @@ public:
             }
             std::cout << "|" << std::endl;
         }
+        std::cout << "\nVertex: " << number_of_vertex << std::endl;
+        std::cout << "Edge: " << number_of_edge << std::endl;
     }    
     void checking_the_connectivity_of_graph()
     {
         std::vector<std::size_t> indexes;
-        if (is_connected(indexes))
+        if (is_connected(indexes, false))
         {
             std::cout << "\nThe graph is connected!" << std::endl;
         }
@@ -155,10 +181,10 @@ public:
             std::cout << "\nThe graph isn't connected!" << std::endl;
         }
     }
-    void depth_first_search()
+    void depth_first_search(bool mode)
     {
         std::vector<std::size_t> indexes;
-        if (is_connected(indexes))
+        if (is_connected(indexes, mode))
         {
             std::cout << "\nSearch is finished!\nVertex: " << std::endl;
             for (std::size_t i = 0; i < number_of_vertex; i++)
@@ -170,19 +196,22 @@ public:
         else
         {
             std::cout << "\nThe graph isn't connected!" << std::endl;
-        }              
+        }      
     }
 };
 int main()
 {
     Graph_matrix graph(6, false);
-    graph.add_edge(0, 1, 10);
-    graph.add_edge(0, 5, 10);
-    graph.add_edge(3, 4, 10);
-    graph.add_edge(3, 2, 10);
-    graph.add_edge(5, 3, 10);
+    graph.add_edge(0, 1, 0);
+    graph.add_edge(0, 2, 7);
+    graph.add_edge(0, 3, 6);
+    graph.add_edge(2, 5, 40);
+    graph.add_edge(3, 4, -7);
+    graph.add_edge(3, 5, 14);
     graph.write_graph();
-    graph.depth_first_search();
+    graph.depth_first_search(false);
+    graph.add_vertex();
+    graph.depth_first_search(true);
 
     return 0;
 }
